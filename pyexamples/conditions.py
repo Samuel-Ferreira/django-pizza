2c9e0c667aa93b5d973b22479580ee15dffffd95
Samuel-Ferreira <samuel.fg96@gmail.com>
2015-08-18 10:50:08 -0300

    Added loops.py

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -1,1 +1,16 @@
 print ('Hello World')
+
+sause = 'tomato'
+
+order_description = """
+	The client asked for pizza with less cheese.
+	Instead of cheese he would like to have more ham.
+"""
+
+if sause.startswith('toma'):
+	print("it's probably tomato...")
+else:
+	print ("not tomato")
+
+if order_description:
+	print(order_description)
