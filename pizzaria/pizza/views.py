d90e5cab156370824f9881d1b2970c1ecdb39468
Samuel-Ferreira <samuel.fg96@gmail.com>
2015-08-18 17:31:16 -0300

    edited views

--- pizzaria/pizza/views.py
+++ pizzaria/pizza/views.py
@@ -3,7 +3,9 @@ from django.shortcuts import render
 
 from .models import Pizza
 
+
 def menu(request):
+    #import ipdb; ipdb.set_trace()
     pizzas = Pizza.objects.all()
     context = {'pizzas': pizzas}
     return render(request, 'pizza/menu.html', context)
