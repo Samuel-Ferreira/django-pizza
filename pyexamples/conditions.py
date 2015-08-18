da40070316c49eda310a1758159d974c44270794
Samuel-Ferreira <samuel.fg96@gmail.com>
2015-08-18 14:25:24 -0300

    Conditons and Loop

--- pyexamples/conditions.py
+++ pyexamples/conditions.py
@@ -3,14 +3,14 @@ print ('Hello World')
 sause = 'tomato'
 
 order_description = """
-	The client asked for pizza with less cheese.
-	Instead of cheese he would like to have more ham.
+    The client asked for pizza with less cheese.
+    Instead of cheese he would like to have more ham.
 """
 
 if sause.startswith('toma'):
-	print("it's probably tomato...")
+    print("it's probably tomato...")
 else:
-	print ("not tomato")
+    print ("not tomato")
 
 if order_description:
-	print(order_description)
+    print(order_description)
--- pyexamples/loops.py
+++ pyexamples/loops.py
@@ -1,14 +1,15 @@
 pedidos = [
-	{
-		'nome': 'Mario',
-		'sabor': 'portuguesa'		
-	},
-	{
-		'nome': 'Marco',
-		'sabor': 'presunto'
-	}
+    {
+        'nome': 'Mario',
+        'sabor': 'portuguesa'
+    },
+    {
+        'nome': 'Marco',
+        'sabor': 'presunto'
+    }
 ]
 
+
 for pedido in pedidos:
-	s = 'Nome: {}\nSabor: {}'
-	print(s.format(pedido['nome'], pedido['sabor']))
+    s = 'Nome: {}\nSabor: {}'
+    print(s.format(pedido['nome'], pedido['sabor']))
